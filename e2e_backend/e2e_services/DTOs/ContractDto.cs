0d3bf328d2500d6cfb5d3ae7f63f6f7eb6a48bd0
irosic21 <rosicivo12@gmail.com>
2025-03-15 17:47:05 +0100

    Implemented Dtos for Employee, Contract and LeaveRecords enteties

---
+++ e2e_backend/e2e_services/DTOs/ContractDto.cs
@@ -0,0 +1,30 @@
+﻿using e2e_API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace e2e_services.DTOs
+{
+    public class CreateContractDto
+    {
+        [JsonConverter(typeof(DateTimeJsonConverter))]
+        public DateTime DateOfEmployment { get; set; }
+        public string ContractType { get; set; }
+        public int DurationOfContract { get; set; } 
+        public string Department { get; set; }
+    }
+    public class ContractDto : CreateContractDto
+    {
+        public int Id { get; set; }
+    }
+    public class UpdateContractDto
+    {
+        public DateTime? DateOfEmployment { get; set; }
+        public string? ContractType { get; set; }
+        public int? DurationOfContract { get; set; }
+        public string? Department { get; set; }
+    }
+}
